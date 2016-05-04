c159262fd9dd2ba386e29d02eb540057d4e9e1f3
InterProFront <fallzem@gmail.com>
2016-05-04 14:00:51 +0600

    fix text

--- resources/views/front/service/service.blade.php
+++ resources/views/front/service/service.blade.php
@@ -9,7 +9,7 @@
         @yield('menu')
         <div class="company company-big">
             <h1 class="big-title">{{$s_study->title_field}}</h1>
-            <div class="full-size-descr">
+            <div class="full-size-descr service">
                 {!! $s_study->about_services_field !!}
             </div>
             <?php $i = 0?>
