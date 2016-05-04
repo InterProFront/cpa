bdcd3e53d32cd71facd8b7f3a488697245526ffd
Konstantin <mr.kaluchii@mail.ru>
2016-05-04 16:43:49 +0600

    Add pict in News page

--- resources/views/back/blocks/dom_news.blade.php
+++ resources/views/back/blocks/dom_news.blade.php
@@ -9,6 +9,45 @@
         </div>
         <div class="field-wrap ">
             <label class="input-file">
+            </label>
+            <div class="image-load">
+                <div class="drag-n-drop">
+                    <div class="drag"><input type="text" value="Отпустите клавишу мыши, чтобы загрузить файл"></div>
+                    <div class="img-hide-block">
+                        <input type="hidden" class="prefix" data-field-name="head" data-field-type="image"
+                               data-block="dom_news" value="{{$dom_news->head_image->prefix}}">
+                        <input type="hidden" class="preview_link" data-field-name="head" data-field-type="image"
+                               data-block="dom_news" value="{{$dom_news->head_image->preview_link}}">
+                        <input type="hidden" class="primary_link" data-field-name="head" data-field-type="image"
+                               data-block="dom_news" value="{{$dom_news->head_image->primary_link}}">
+                        <input type="hidden" class="secondary_link" data-field-name="head" data-field-type="image"
+                               data-block="dom_news" value="{{$dom_news->head_image->secondary_link}}">
+                        <input type="hidden" class="icon_link" data-field-name="head" data-field-type="image"
+                               data-block="dom_news" value="{{$dom_news->head_image->icon_link}}">
+                    </div>
+                    <div class="preview-block">
+                        <img src="/images/{{$dom_news->head_image->preview_link}}" class="preview"
+                             data-field-name="head" data-block="dom_news">
+                        <div class="buttons">
+                            <span class="refresh"></span>
+                            <span class="clear"></span>
+                        </div>
+                    </div>
+                    <div class="action-block">
+                        <input type="text" placeholder="alt текст" class="alt-text"
+                               value="{{$dom_news->head_image->alt}}" data-field-name="head"
+                               data-block="dom_news">
+                        <label class="file-input">
+                            Загрузить
+                            <input type="file" accept="image/*" class="input_file block_field" data-entity="block"
+                                   data-field-name="head" data-field-type="image" data-block="dom_news">
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="field-wrap ">
+            <label class="input-file">
                 <ul class="group-block group_container" data-block="dom_news" data-group="news" data-owner-id="0">
                     @foreach($dom_news->news_group as $item_news )
                         @include('back.blocks.groupitems.dom_news.news')
