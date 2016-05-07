6b99606ed67caa1063657050259c9fc19597c3f4
Konstantin <mr.kaluchii@mail.ru>
2016-05-07 14:13:01 +0600

    Del button fix

--- resources/views/back/blocks/dom_study.blade.php
+++ resources/views/back/blocks/dom_study.blade.php
@@ -1,44 +1,44 @@
-    <div class="block ">
-        <div class="field-wrap ">
-            <label class="input-file">
-                Список курсов обучения
-            </label>
-            <table>
-                <thead>
-                <tr>
-                    <td>Название</td>
-                    <td></td>
-                    <td>Публикации</td>
-                    <td>Сортировка</td>
-                    <td>Редактировать</td>
-                    <td>Удалить</td>
-                </tr>
-                </thead>
-                <tbody class="group-block group_container" data-block="dom_study" data-group="course" data-owner-id="0">
-                @foreach($dom_study->course_group as $item_course )
-                    @include('back.blocks.groupitems.dom_study.course')
-                @endforeach
-                </tbody>
-                <tfoot>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>
-                        <button class="any_create" data-block="dom_study" data-group="course"
-                                data-descr="Эл. первой группы" data-owner-id="0"> Добавить
-                        </button>
-                    </td>
-                </tr>
-                </tfoot>
-            </table>
-        </div>
-        <div class="field-wrap buttons">
-            <button class="btn btn-primary pull-right any_save" data-block="dom_study" data-entity="block"
-                    data-descr="">
-                <span class="save_button">Сохранить</span>
-            </button>
-        </div>
+<div class="block ">
+    <div class="field-wrap ">
+        <label class="input-file">
+            Список курсов обучения
+        </label>
+        <table>
+            <thead>
+            <tr>
+                <td>Название</td>
+                <td></td>
+                <td>Публикации</td>
+                <td>Сортировка</td>
+                <td>Редактировать</td>
+                <td>Удалить</td>
+            </tr>
+            </thead>
+            <tbody class="group-block group_container" data-block="dom_study" data-group="course" data-owner-id="0">
+            @foreach($dom_study->course_group as $item_course )
+                @include('back.blocks.groupitems.dom_study.course')
+            @endforeach
+            </tbody>
+            <tfoot>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                    <button class="any_create" data-block="dom_study" data-group="course"
+                            data-descr="Эл. первой группы" data-owner-id="0"> Добавить
+                    </button>
+                </td>
+            </tr>
+            </tfoot>
+        </table>
     </div>
+    <div class="field-wrap buttons">
+        <button class="btn btn-primary pull-right any_save" data-block="dom_study" data-entity="block"
+                data-descr="">
+            <span class="save_button">Сохранить</span>
+        </button>
+    </div>
+</div>
