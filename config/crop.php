9296a535dcf2d7594e3b77e0d164faf10e97fb11
InterProFront <fallzem@gmail.com>
2016-05-13 19:11:12 +0600

    fix crops size

--- config/crop.php
+++ config/crop.php
@@ -4,5 +4,9 @@ return [
     'service_image_serv_pict' => [
         'big'=>['width'=>230, 'height'=>230, 'man'=>'primary', 'target'=>'primary'],
         'small'=>['width'=>110, 'height'=>110, 'man'=>'primary', 'target'=>'primary']
+    ],
+    'course_image_course_pict' => [
+        'big'=>['width'=>230, 'height'=>230, 'man'=>'primary', 'target'=>'primary'],
+        'small'=>['width'=>110, 'height'=>110, 'man'=>'primary', 'target'=>'primary']
     ]
 ];
