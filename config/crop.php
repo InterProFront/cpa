43b8a78ebb8e9d8a26488d175d723a9ed92b7aeb
InterProFront <fallzem@gmail.com>
2016-05-13 18:35:52 +0600

    fix crops size

--- config/crop.php
+++ config/crop.php
@@ -3,6 +3,6 @@
 return [
     'service_image_serv_pict' => [
         'big'=>['width'=>230, 'height'=>230, 'man'=>'primary', 'target'=>'primary'],
-        'small'=>['width'=>200, 'height'=>150, 'man'=>'primary', 'target'=>'primary']
+        'small'=>['width'=>110, 'height'=>110, 'man'=>'primary', 'target'=>'primary']
     ]
 ];
