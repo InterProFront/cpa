73defe9311ae1a54273a670736c114de532b8f6c
InterProFront <fallzem@gmail.com>
2016-05-13 19:16:42 +0600

    fix stady

--- resources/views/front/study/photo.blade.php
+++ resources/views/front/study/photo.blade.php
@@ -4,27 +4,27 @@
     <?php $c++ ?>
         @if($c == 1)
             <div class="col-2-3">
-                <a href="/images/{{$img->course_pict_image->primary_link}}"><img src="/images/{{$img->course_pict_image->secondary_link}}" alt=""></a>
+                <a href="/images/{{$img->course_pict_image->primary_link}}"><img src="/images/{{$img->course_pict_image->big_crop->link}}" alt=""></a>
             </div>
         @elseif($c == 2)
             <div class="col-1-3">
-                <a href="/images/{{$img->course_pict_image->primary_link}}"><img src="/images/{{$img->course_pict_image->icon_link}}" alt=""></a>
+                <a href="/images/{{$img->course_pict_image->primary_link}}"><img src="/images/{{$img->course_pict_image->small_crop->link}}" alt=""></a>
         @elseif($c == 3)
-                <a href="/images/{{$img->course_pict_image->primary_link}}"><img src="/images/{{$img->course_pict_image->icon_link}}" alt=""></a>
+                <a href="/images/{{$img->course_pict_image->primary_link}}"><img src="/images/{{$img->course_pict_image->small_crop->link}}" alt=""></a>
             </div>
         @elseif($c == 4)
             </div>
             <div class="row">
                 <div class="col-1-3">
-                    <a href="/images/{{$img->course_pict_image->primary_link}}"><img src="/images/{{$img->course_pict_image->icon_link}}" alt=""></a>
+                    <a href="/images/{{$img->course_pict_image->primary_link}}"><img src="/images/{{$img->course_pict_image->small_crop->link}}" alt=""></a>
         @elseif($c == 5)
         </div>
         <div class="col-1-3">
-            <a href="/images/{{$img->course_pict_image->primary_link}}"><img src="/images/{{$img->course_pict_image->icon_link}}" alt=""></a>
+            <a href="/images/{{$img->course_pict_image->primary_link}}"><img src="/images/{{$img->course_pict_image->small_crop->link}}" alt=""></a>
         @elseif($c == 6)
             </div>
             <div class="col-1-3">
-                <a href="/images/{{$img->course_pict_image->primary_link}}"><img src="/images/{{$img->course_pict_image->icon_link}}" alt=""></a>
+                <a href="/images/{{$img->course_pict_image->primary_link}}"><img src="/images/{{$img->course_pict_image->small_crop->link}}" alt=""></a>
             </div>
             </div>
         @endif
