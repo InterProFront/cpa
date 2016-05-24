d939cd2ab51ad91720d9001e272b5e92452d1e6a
InterProFront <fallzem@gmail.com>
2016-05-24 09:59:33 +0600

    add mail template

---
+++ resources/views/back/mail/serv.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <h1>Заявка на услугу</h1>
+    <p>{{$item->fio_field}}</p>
+    <p>{{$item->email_field}}</p>
+    <p>{{$item->phone_field}}</p>
+    <p>{{$item->service_field}}</p>
+</div>
